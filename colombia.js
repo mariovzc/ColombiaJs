e3f9818f58ca6da1398aeda84a015996ceefea1b
Mario Vizcaino <mario.vizcainos@gmail.com>
2016-05-20 10:32:51 -0500

    version update

--- colombia.js
+++ colombia.js
@@ -1,5 +1,5 @@
 /*
- colombia.js v 1.0
+ colombia.js v 1.0.1
  STATES OBJECT id , NAME
  cities OBJECT city_id, name, state_id, state_name
   MIT License. Copyright 2016 Mario Vizcaino. https://github.com/mariovzc
